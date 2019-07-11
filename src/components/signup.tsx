ed96501783a20afc673d9e02ca17d5559189f3a5
Nick Salter <thisislsbdc@gmail.com>
2019-07-11 12:02:56 -0500

    added SignUp component

---
+++ src/components/signup.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Signup extends Component{
+
+        constructor(props: any){
+                super(props);
+
+                this.state = {
+                        job: '',
+                        fname: ''
+
+                }
+        }
+
+
+}
