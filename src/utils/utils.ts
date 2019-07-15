512dc3e76366856cc1d4eb0385d13fa1428f7f90
Aayush <aayush6194@gmail.com>
2019-07-15 14:38:21 -0500

    Regex Input Validation added

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,5 +14,29 @@ function getLocalStorage(key: string) {
     return localStorage.getItem(key);
   }
 }
+function nameTest(name: String){
+  const nameRegex = /^[a-zA-Z]{2,20}$/;
+  return nameRegex.test(name);
+}
 
-export { setLocalStorage, getLocalStorage};
+function usernameTest(name: String){
+  const usernameRegex = /^[a-zA-Z0-9]{8,30}$/;
+  return usernameRegex.test(name);
+}
+
+function emailTest(name: String){
+  const emailRegex = /\S+@\S+\.\S+/;
+  return emailRegex.test(name);
+}
+
+function passwordTest(name: String){
+  const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{7,30})$/;
+  return passwordRegex.test(name);
+}
+
+function userTest (user: Object){
+    let { firstName, lastName, username, password, email} = user;
+    return nameTest(firstName + lastName) && userTest(username) && emailTest (email) && passwordTest (password);
+}
+
+export {setLocalStorage, getLocalStorage , nameTest, usernameTest, emailTest, passwordTest, userTest};
