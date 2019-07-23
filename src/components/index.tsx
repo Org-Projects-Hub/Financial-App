8ecc06f9bcbddba53d9b2ae6d662d003eb0b25c7
lilaht <lilahthibodeaux@gmail.com>
2019-07-23 18:58:30 -0500

    create posttest, added to simulation

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ import Header from './Header';
 import Loader from './Loader';
 import Navbar from './Navbar';
 import Pretest from './Pretest';
+import Posttest from './Posttest';
 import SettingItem from './SettingItem'
-export {Header, Loader, Navbar, Pretest, SettingItem};
+export {Header, Loader, Navbar, Pretest, SettingItem, Posttest};
--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import {Pretest} from '../components'
+import {Pretest, Posttest} from '../components'
 const Wrapper = styled.div`
   background-image: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
   width: 100%;
@@ -11,10 +11,12 @@ const Wrapper = styled.div`
 const Simulation =({userName}:{userName: string})=>
 {
   const [stage, setStage] = useState("pretest");
+  
   return (
       <Wrapper className="center">
         <div className="top">Hello, {userName}</div>
-        {stage === "pretest" && <Pretest />}
+        {stage === "pretest" && <Pretest setStage={setStage} />}
+        {stage === "posttest" && <Posttest setStage={setStage} />}
       </Wrapper>)
     };
 
