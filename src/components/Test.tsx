5c954bcc1a13a7451098fe69974d9402ec55d831
Aayush <aayush6194@gmail.com>
2019-07-23 12:12:53 -0500

    Styling

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import Tests from './Tests.json';
 import Question from './Question';
 import { getThemeProps } from '@material-ui/styles';
+import {Card} from '../style/styled';
 
 const Test = (props: any)=>
 {
 
   if(props.testType === 'pretest' || props.testType === 'posttest') {
     return(
-      <div>
+      <Card>
         {Tests.testType.pretest.questions.map((question, i) =>
           <Question {...question} answers={Tests.testType.pretest.answers} key={i}/>)}
-      </div>
+      </Card>
     );
   } else {
 
