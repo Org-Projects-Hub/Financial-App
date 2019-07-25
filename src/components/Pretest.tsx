b04bc99dc0424d64fcb585f0e08e6eea92a93e68
Aayush <aayush6194@gmail.com>
2019-07-25 12:32:03 -0500

    responsive design with minMax

--- src/components/Pretest.tsx
+++ src/components/Pretest.tsx
@@ -9,7 +9,7 @@ const Div = styled.div`
     padding: 15px;
 `;
 
-const PretestExplanation: string = 'Pretest explanation';
+const PretestExplanation: string = '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.';
 
 type Props = {
     begin: boolean,
@@ -37,7 +37,7 @@ export default class Pretest extends React.Component<{setStage: Function}, Props
     return(
           !begin?
                 <GridRow rows="2">
-                    <Card>{PretestExplanation}</Card>
+                    <Card style={{maxWidth:"700px", width: "90%" }}>{PretestExplanation}</Card>
                     <Div><button className="btn" onClick={this.onClick}>Begin Pretest</button></Div>
                 </GridRow>
                :
