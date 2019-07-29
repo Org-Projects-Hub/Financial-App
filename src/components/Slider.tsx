ebe499c60f51d62a014ec71a661b9af2b6cade50
Nick Salter <thisislsbdc@gmail.com>
2019-07-29 15:46:51 -0500

    added CSS grid to homepage and Slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,41 +2,56 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-background: black; 
-width:100%;
+display: grid;
+grid-template-columns: repeat(3, 1fr);;
+grid-template-rows: repeat(3, 1fr);
+align-items:stretch;
+justify-items:stretch;
+height:100%;
 `;
 
-const Pic = styled.img`
-position:absolute;
-width:75%;
-height:40%;
-transform: translate(-50%, -50%);
+const Pic = styled.div`
+grid-column: 1 / span 3;
+grid-row: 1 / span 3;
+justify-self: stretch;
+align-self: stretch;
+height:100%;
+width:100%;
+border-style: solid;
 `;
 
 const Left = styled.a`
-position: absolute;
-left:25%;
-
+grid-column: 1 / span 1;
+grid-row: 2 / span 1;
+justify-self: start;
+align-self: center;
+font-size:25pt;
 `;
 
 const Right = styled.a`
-position: absolute;
-right:25%
+grid-column: 3 / span 1;
+grid-row: 2 / span 1;
+justify-self: end;
+align-self: center;
+font-size:25pt;
 `;
+
 
 const Slider = ()=> {
 
     return(
 
         <Wrapper>
-            <Pic src="https://americasaves.org/images/article/Dollar%20Bills.jpg"/>
+            <Pic>
+                <img style={{height:"100%", width:"100%"}}src="https://americasaves.org/images/article/Dollar%20Bills.jpg"/ >
+            </Pic>
             
             <Left>
-                <i className="fas fa-arrow-left"></i>
+                <i className="fas fa-arrow-left fa-lg"></i>
             </Left>
 
             <Right>
-                <i className="fas fa-arrow-right"></i>
+                <i className="fas fa-arrow-right fa-lg"></i>
             </Right>
 
         </Wrapper>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,41 +4,53 @@ import banner from '../assets/images/web banner.png';
 import logo from '../assets/images/unitedWayLogoShadow.png';
 import {Slider} from '../components';
 
+
 const Wrapper = styled.div`
   background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
   height: 100vh;
   text-align: center
-  z-index:-1;`;
+  display: grid;
+  min-height:100vh;
+  grid-template-columns: repeat(5, 1fr);;
+  grid-template-rows: 15% repeat(4, 1fr);
+  align-items: stretch;
+  place-items: stretch;
+  `;
 
 
 const Banner = styled.img`
-top:0;
-left:0;
-position:absolute;
-z-index:0;
-width:100%;
-height:28%;
-border-bottom:
+grid-column: 1 / span 5;
+grid-row: 1 / span 1;
+justify-self: stretch;
+align-self: stretch;
 `;
 
-const Logo = styled.img`
-position:absolute;
-top:12%;
-left: 50%;
-transform: translate(-50%, -50%);
+const Logo = styled.a`
+grid-column: 3 / span 1;
+grid-row: 1 / span 1;
+justify-self: stretch;
+align-self: stretch;
 z-index:1;
 `;
 
+const SlidePos = styled.div`
+grid-column: 2 / span 3;
+grid-row: 2 / span 2;
+justify-self: stretch;
+align-self: stretch;
+`;
 
 const Home = ({userName}:{userName: string})=>
-  <Wrapper>
+<Wrapper>
+    
+      <Banner src={banner}/>
 
-    <Banner src={banner} />
+      <Logo href="http://www.unitedwaynela.org/"> <img style={{height:"90%"}} src={logo} /></Logo>
 
-    <a href="http://www.unitedwaynela.org/"> <Logo src={logo} /></a>
+      <SlidePos>
+        <Slider/>
+      </SlidePos>
 
-    <Slider/>
-
-  </Wrapper>
+</Wrapper>
 
 export default Home;
