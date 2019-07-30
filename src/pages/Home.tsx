06cfda866843bcbf84b808d7b020aa60f979bbe9
Nick Salter <thisislsbdc@gmail.com>
2019-07-29 22:30:28 -0500

    added simulation button to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import banner from '../assets/images/web banner.png';
 import logo from '../assets/images/unitedWayLogoShadow.png';
 import {Slider} from '../components';
 import {Card} from '../style/styled';
+import {Link} from "react-router-dom";
+
 
 
 const Wrapper = styled.div`
@@ -20,17 +22,19 @@ const Wrapper = styled.div`
 
 
 const Banner = styled.img`
-grid-column: 1 / span 5;
+grid-column: 2 / span 3;
 grid-row: 1 / span 1;
 justify-self: stretch;
 align-self: stretch;
 `;
 
 const Logo = styled.a`
-grid-column: 1 / span 1;
+grid-column: 2 / span 1;
 grid-row: 1 / span 1;
-justify-self: stretch;
+justify-self: start;
 align-self: stretch;
+padding-left: 2vh;
+object-fit: contain;
 z-index:1;
 `;
 
@@ -46,16 +50,30 @@ grid-column: 2 / span 2;
 grid-row: 4 / span 2;
 align-self: stretch;
 justify-self: stretch;
+font-family: 'Capriola', sans-serif;
+font-weight:bolder;
+color: rgb(183, 191, 23);
 `;
 
+const Button = styled.button`
+border-radius: 0.5em;
+font-size: 120%;
+height:7vh;
+width:18vh;
+grid-column: 4 / span 1;
+grid-row: 4 / span 1;
+justify-self: end;
+align-self:end;
+`;
 
 const Home = ({user}:{user: any})=>
 
 <Wrapper>
+
     
       <Banner src={banner}/>
 
-      <Logo href="http://www.unitedwaynela.org/"> <img style={{height:"90%"}} src={logo} /></Logo>
+      <Logo href="http://www.unitedwaynela.org/"> <img style={{height:"90%", objectFit: "contain"}} src={logo} /></Logo>
 
       <SlidePos>
         <Slider/>
@@ -66,6 +84,8 @@ const Home = ({user}:{user: any})=>
           Here is a description of Dollars and $ense, what this app is about, why it was created, and what you will learn.
         </Card>
       </Desc>
+
+      <Button><Link to="/">To Simulation</Link></Button>
 
 </Wrapper>
 
