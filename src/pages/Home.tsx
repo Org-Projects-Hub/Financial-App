43901c38326a26e72ff1fe67319f320323d554b7
Nick Salter <thisislsbdc@gmail.com>
2019-07-29 23:43:40 -0500

    added card background to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,17 @@ import {Link} from "react-router-dom";
 
 
 const Wrapper = styled.div`
-  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
   height: 100vh;
   text-align: center
   display: grid;
   min-height:100vh;
-  grid-template-columns: repeat(5, 1fr);;
+  grid-template-columns: 10vh repeat(3, 1fr) 10vh;
   grid-template-rows: 15% repeat(4, 1fr);
   align-items: stretch;
   place-items: stretch;
+  box-shadow: 0px 0px 20px 5px gray;
+  margin: 0 15%;
+  background-color: #f1f1f1;
   `;
 
 
@@ -43,6 +45,7 @@ grid-column: 2 / span 3;
 grid-row: 2 / span 2;
 justify-self: stretch;
 align-self: stretch;
+box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
 `;
 
 const Desc = styled.div`
@@ -52,14 +55,14 @@ align-self: stretch;
 justify-self: stretch;
 font-family: 'Capriola', sans-serif;
 font-weight:bolder;
-color: blue;
+color: FAB084;
 `;
 
 const Login = styled.div`
 grid-column: 4 / span 1;
 grid-row: 4 / span 1;
-justify-self: end;
-align-self:end;
+justify-self: center;
+align-self: end;
 `;
 
 const Button = styled.button`
@@ -71,8 +74,16 @@ background-color: rgb(0, 40, 133);
 }
 `;
 
-const Home = ({user}:{user: any})=>
+const Border = styled.div`
+    margin: 0px;
+    min-height: 100vh;
+    background-color: #F1F1F1;
+    
+`;
 
+
+const Home = ({user}:{user: any})=>
+<Border>
 <Wrapper>
 
     
@@ -81,11 +92,11 @@ const Home = ({user}:{user: any})=>
       <Logo href="http://www.unitedwaynela.org/"> <img style={{height:"90%", objectFit: "contain"}} src={logo} /></Logo>
 
       <SlidePos>
-        <Slider/>
+         <Slider/>
       </SlidePos>
 
       <Desc>
-        <Card style={{height: "90%"}}>
+        <Card style={{height: "90%", backgroundColor: "#84CEFA"}}>
           Here is a description of Dollars and $ense, what this app is about, why it was created, and what you will learn.
         </Card>
       </Desc>
@@ -99,5 +110,6 @@ const Home = ({user}:{user: any})=>
       </Login>
 
 </Wrapper>
+</Border>
 
 export default Home;
