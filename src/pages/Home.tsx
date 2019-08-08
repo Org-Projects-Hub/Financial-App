9cfa54130f2eb1015e4540dadfd5cd1927aace9e
Nick Salter <thisislsbdc@gmail.com>
2019-08-08 13:57:03 -0500

    added admin panel button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from '../assets/images/unitedWayLogoShadow.png';
 import {Card} from '../style/styled';
+import styled from 'styled-components'
 import {Wrapper, Banner, Logo, Desc, LoginPOS, Button} from '../style/home';
+import {NavLink} from 'react-router-dom';
 
 
 
+const Admin = styled.div`
+grid-column: 3 / span 1;
+grid-row: 4 / span 1;
+`;
 
 const Home = ({user}:{user: any})=>
 <Wrapper>
@@ -21,6 +27,11 @@ const Home = ({user}:{user: any})=>
       <LoginPOS>
         <Button>TO SIMULATION</Button>
       </LoginPOS>
+      <Admin>
+        <button className="btn">
+        <NavLink to='/admin-pannel'>ADMIN PANEL</NavLink>
+        </button>
+      </Admin>
 
 </Wrapper>
 
