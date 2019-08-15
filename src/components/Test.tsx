c06120c21b58321227fac66dda75a3d3d1ad1b11
lilaht <lilahthibodeaux@gmail.com>
2019-08-15 15:13:24 -0500

    preposttest functionality change

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,35 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tests from './Tests.json';
 import Question from './Question';
-import { getThemeProps } from '@material-ui/styles';
-import {Card} from '../style/styled';
+import { Card, Grid } from '../style/styled';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
 /**
  * Test.tsx
  *
- * @desc: Called by [PrePostTest].  Determines which test will be loaded based on testType prop.
- * @param {any} props - contains prop(s): testType - determines which test questions/answers to render.
+ * @desc: Called by [PrePostTest].  Determines which test will be loaded based on testType prop
+ * @param {string}   testType        determines which test questions/answers to render
+ * @param {Function} setTestComplete sets testComplete boolean true in PrePostTest on test submission
  * @return TSX to be rendered.
  */
 
-const Test = (props: any)=>
-{
-  if(props.testType === 'pretest' || props.testType === 'posttest') {
-    const questions = props.testType === 'pretest'?  Tests.testType.pretest.questions : Tests.testType.posttest.questions;
-    const answers = props.testType === 'pretest'?  Tests.testType.pretest.answers : Tests.testType.posttest.answers;
-    return(
-      <>   <ReactCSSTransitionGroup
-         transitionName="slide"
-         transitionAppear={true}
-         transitionAppearTimeout={8000}
-         transitionEnterTimeout={800}
-         transitionLeaveTimeout={300}
-         transitionEnter={true}
-         transitionLeave={false}>
-          {questions.map((question, i) =>
-          <Question {...question} answers={answers} key={i}/>)}
-           </ReactCSSTransitionGroup>
-      </>
+type Props = {
+  testType: string,
+  setTestComplete: Function
+}
+
+const Test = ({testType, setTestComplete}: Props)=> {
+  const [qNum, setQNum] = useState(0);
+  const [selections, setSelections] = useState([]);
+  const [answered, setAnsered] = useState(false);
+  const [clicked, setClicked] = useState(false)
+  
+  if(testType === 'pretest' || testType === 'posttest') {
+    const questions = testType === 'pretest'?  Tests.testType.pretest.questions : Tests.testType.posttest.questions;
+    const answers = testType === 'pretest'?  Tests.testType.pretest.answers : Tests.testType.posttest.answers;
+    
+    /**
+     * Called when answer selected, updates states, saves user selection
+     * @param id    question # associated w question
+     * @param value user's selection
+     */
+    const nextQuestion = (id: string, value: string) => {
+      if(selections[qNum] === undefined) { /** if there is no answer for current question, add answer to selections array */
+        setSelections([...selections, {
+          id: id,
+          value: value
+        }])
+      } else {  /** if question has been answered, but user went back to change answer */
+        let tempArray = [...selections];
+        tempArray[qNum].id = id;
+        tempArray[qNum].value = value;
+        setSelections(tempArray);
+      }
+
+      if(selections.length < questions.length) { /** increment question number */
+        setQNum(qNum => qNum+1);
+      }
+    }
+
+    const prevQuestion = () => {
+      setQNum(qNum => qNum-1);
+    }
+    
+    while(qNum < questions.length){ /** render questions until all have been answered */
+      return(
+        <div>
+          <Question 
+            id={questions[qNum].id.toString()} 
+            question={questions[qNum].q} 
+            answers={answers} 
+            value={selections[qNum] === undefined? null : selections[qNum].value} 
+            nextQuestion={nextQuestion} />
+
+          {qNum > 0 && <button className="btn" onClick={(e) => prevQuestion()}>Previous</button>} {/** if there is a previous question, display back button */}
+          {selections[qNum] !== undefined && <button className="btn" onClick={(e) => setQNum(qNum => qNum+1)}>Next</button>} {/** if current question has answer, show next button */}
+          {console.log(qNum)}
+        </div>
+      );
+    }
+
+    return (
+      <Grid cols="1">
+        <Card>{testType.charAt(0).toUpperCase() + testType.slice(1)} complete.</Card>
+        <button className="btn" onClick={(e) => setTestComplete(true)}>Submit</button>
+      </Grid>
     );
   }
   else {
