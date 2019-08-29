bc32f5f5c779ebc8893630211633fc04bfd3d182
Aayush <aayush6194@gmail.com>
2019-08-29 11:17:13 -0500

    loader added for promises

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router,  Route, Switch } from "react-router-dom";
 import './App.css';
 import './style/animations.css';
-import {Navbar, Modal} from './components';
+import {Navbar, Modal , Loader} from './components';
 import {Home, Setting, Simulation, Startpage, Signup, UserStartPage, ClassDashboard, AdminPanel} from './pages';
 import api from './api';
 import { setLocalStorage } from './utils/utils';
@@ -21,27 +21,19 @@ export default class App extends React.Component <{}, Props>{
     if(!this.state.tokenChecked){
       api.auth()
       .then((res)=> {
-        if(res.success){
-            this.setState({loggedin: true});
-            this.setState({user: res.user});
-              this.setState({modal: true});
-                      }})
+        if(res.success) this.setState({tokenChecked: true, loggedin : true, user : res.user});
+        })
       .catch((err)=>alert(err));
     }
-
-    this.setState({tokenChecked: true});
-
   }
 
   render(){
     const login = (e: any) => {
       api.login(e)
         .then(res => {
-          console.log(res)
           if (res.success) {
-            this.setState({ loggedin: true });
+            this.setState({ loggedin: true, user: res.user });
             setLocalStorage("token", res.token);
-            this.setState({user: res.user})
           } else {
             alert(res.message);
           }
@@ -51,13 +43,12 @@ export default class App extends React.Component <{}, Props>{
 
     const loggedin = (token : string, user: object) => {
         setLocalStorage("token", token);
-        this.setState({user: user})
-        this.setState({ loggedin: true })
+        this.setState({user: user, loggedin: true});
     }
 
     const logout = () => {
         setLocalStorage("token", "");
-        this.setState({ loggedin: false })
+        this.setState({ loggedin: false });
     }
 
     const close = ()=>{
@@ -65,32 +56,31 @@ export default class App extends React.Component <{}, Props>{
     }
 
     return(
-        <Router>
-            {this.state.loggedin ?
-              <div className={this.state.showNav? "grid-main": ""}>
-            <Navbar showNav={this.state.showNav} hide={()=>{this.setState({showNav: !this.state.showNav})}}/>
-            {/* this.state.modal && <Modal text={`Welcome to Fin App! Head on to the Simulation when you are ready`} close={close} /> */}
-            <Switch>
-
-            <Route path="/classes" render={() => <UserStartPage user={this.state.user}/>} />
-            <Route path="/classDashboard" render={() => <ClassDashboard user={this.state.user}/>} />
-            <Route path="/Simulation" render={()=> <Simulation user={this.state.user} />} />
-            <Route path="/setting" render={()=> <Setting logout={logout} user = {this.state.user}/>} />
-            <Route path="/admin-pannel" render={()=> <AdminPanel />} />
-            <Route path="/" render={()=> <Home user={this.state.user} />} />
-
-            </Switch>
-            </div>
-           :<Switch>
-            <Route path="/signup" render={() => <Signup loggedin={loggedin} />} />
-
-            <Route render={()=> <Startpage login={login}  loggedin={loggedin}/>} />
-            
-
-            </Switch>
+          <>{this.state.tokenChecked?
+                <Router>
+                    {this.state.loggedin ?
+                      <div className={this.state.showNav? "grid-main": ""}>
+                        <Navbar showNav={this.state.showNav} hide={()=>{this.setState({showNav: !this.state.showNav})}}/>
+                        <Switch>
+                          <Route path="/classes" render={() => <UserStartPage user={this.state.user}/>} />
+                          <Route path="/classDashboard" render={() => <ClassDashboard user={this.state.user}/>} />
+                          <Route path="/Simulation" render={()=> <Simulation user={this.state.user} />} />
+                          <Route path="/setting" render={()=> <Setting logout={logout} user = {this.state.user}/>} />
+                          <Route path="/admin-pannel" render={()=> <AdminPanel />} />
+                          <Route path="/" render={()=> <Home user={this.state.user} />} />
+                        </Switch>
+                    </div>
+                   :
+                   <Switch>
+                      <Route path="/signup" render={() => <Signup loggedin={loggedin} />} />
+                      <Route render={()=> <Startpage login={login}  loggedin={loggedin}/>} />
+                    </Switch>
+                  }
+              </Router>
+                :
+              <Loader />
           }
-
-        </Router>
-);
+        </>
+      );
     }
 }
--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useInterval from '@use-it/interval';
 import {Wrapper, Card, Grid} from '../style/styled';
 import { SelectInput} from '../components';
 import styled from 'styled-components';
@@ -6,94 +7,18 @@ import { setLocalStorage } from '../utils/utils';
 import { Chart } from "react-google-charts";
 import Loader from '../components/Loader';
 const AdminPanel = ()=>{
+
  const options = ["All Users", "Age", "Gender", "Grade"];
+const [pointer, setPointer] = useState(0);
+
+  useInterval( ()=>{
+  console.log(pointer);
+  setPointer( pointer + 1 );
+}, 1000);
+
     return (
       <Wrapper className="full-height">
-       <div className="container" >
-       <Grid cols="1" >
-        <div className="selection-menus">
-          <select className="btn">
-            <option value="all">All Users</option>
-            <option value="org">Organizations</option>
-            <option value="ind">Individuals</option>
-          </select>
-        </div>
-        <SelectInput style={{width: "50%"}} name={"name"} arr={options} required={true} action={()=>{console.log()}} />
-
-        <Card className="full-width" >
-          <Grid cols="3" >
-            <span className="pointer bold active-tab" >Test Scores</span>
-            <span className="pointer bold">Debt</span>
-            <span className="pointer bold">Test Question</span>
-          </Grid>
-        </Card>
-
-
-        <Card className="full-width" style={{minHeight: "10em"}} >
-        <Chart
-          width={'100%'}
-          style={{maxWidth:"80vw"}}
-          height={'300px'}
-          chartType="BarChart"
-          loader={<Loader />}
-          data={[
-            ['Demographics', 'Pretest', 'PostTest'],
-            ['Freshmen', 80, 35],
-            ['Sophomore', 95, 20],
-            ['Juniors', 78, 30],
-            ['Senior', 85, 5],
-            ['Others', 40, 70],
-          ]}
-          options={{
-            title: 'PreTest Vs PostTest',
-            chartArea: { width: '50%' },
-            hAxis: {
-              title: 'Scores',
-              minValue: 0,
-            },
-            vAxis: {
-              title: 'Demographics',
-            },
-          }}
-          // For tests
-          rootProps={{ 'data-testid': '1' }}
-        />
-
-        </Card>
-
-        <Card className="full-width" style={{minHeight: "10em"}} >
-        <Chart
-          width={'100%'}
-          style={{maxWidth:"80vw"}}
-          height={'300px'}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ['Demographics', 'Pretest', 'PostTest'],
-            ['Freshmen', 80, 35],
-            ['Sophomore', 95, 20],
-            ['Juniors', 78, 30],
-            ['Senior', 85, 5],
-            ['Others', 40, 70],
-          ]}
-          options={{
-            title: 'PreTest Vs PostTest',
-            chartArea: { width: '80%' },
-            hAxis: {
-              title: 'Scores',
-              minValue: 0,
-            },
-            vAxis: {
-              title: 'Demographics',
-            },
-          }}
-          // For tests
-          rootProps={{ 'data-testid': '1' }}
-        />
-
-        </Card>
-      </Grid>
-       </div>
+        {options[pointer]}
       </Wrapper>
     );
   }
