cffac4f5a23f3b438d99593bfd3ff7fcc21db848
Aayush <aayush6194@gmail.com>
2019-08-29 11:58:16 -0500

    api request changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,81 +1,35 @@
 import { getLocalStorage, setLocalStorage } from '../utils/utils';
+import { get, authGet, post, authPost, authPut } from './request';
 
- const URL: any = "https://finapp.aayushh.com";
-//
-//
- const API = {
-   login: URL + "/login",
-   signup: URL + "/signup",
-   auth: `${URL}/auth`,
-   answer: `${URL}/answer`,
-   edit: `${URL}/edit`,
-   addClass: `${URL}/add-class`,
-   getClass: `${URL}/class`,
+const URL : string = "https://finapp.aayushh.com";
+const API : any = {
+  login: URL + "/login",
+  signup: URL + "/signup",
+  auth: `${URL}/auth`,
+  edit: `${URL}/edit`,
+  getClass: `${URL}/class`,
+
+   //teacher
+   createClass: `${URL}/create-class`,
    getStudent: `${URL}/students`,
-   createClass: `${URL}/create-class`
+
+   //Students
+   addClass: `${URL}/add-class`,
+   answer: `${URL}/answer`,
+   getAnswers: `${URL}/answer`
  }
 
-function header() {
-  return { "Content-Type": "application/json" };
-}
-
-function authHeader() {
-  let authtoken = getLocalStorage("token");
-  console.log(authtoken)
-  return { "Content-Type": "application/json", token: authtoken };
-}
-
-
-
-function get(url: string) {
-  return fetch(url, { method: 'GET', headers: header() })
-    .then(response => response.json());
-}
-
-function authGet(url: string) {
-  return fetch(url, { method: 'GET', headers: authHeader() })
-    .then(response => response.json());
-}
-
-
-function post(url: string, body: object) {
-    return fetch(url, { method: 'POST', headers: header(), body: JSON.stringify(body) })
-      .then(response => response.json());
-}
-//
-// function headerPost(url, body, header) {
-//     return fetch(url, { method: 'POST', headers: { "Content-Type": "application/json", header }})
-//       .then(response => response.json());
-// }
-//
-function authPost(url : string, body : any) {
-  return fetch(url, { method: 'POST', headers: authHeader(), body: JSON.stringify(body) })
-  .then(response => response.json());
-}
-
-// function put(url, body) {
-//   //todo
-// }
-//
-function authPut(url : string, body : any) {
-  return fetch(url, { method: 'PUT', headers: authHeader(), body: JSON.stringify(body) })
-  .then(response => response.json());
-}
-
-//
 export default {
-  login: function({email, password}: any) {
-    return post(API.login, {email, password });
+  login: function({ email, password }: any) {
+    return post(API.login, { email, password });
   },
-
   signup: function({email, password, firstName, lastName, username}: any) {
     return post(API.signup, {email, password, firstName, lastName, username});
   },
-
   auth: function(){
     return authPost(API.auth,{});
   },
-  answer: function({q_id, typesType, answer} : any){
+  answer: function({ q_id, typesType, answer } : any){
     return authPut(API.answer, {q_id, typesType, answer});
   },
   addClass: function({code} : any){
@@ -87,7 +41,7 @@ export default {
   getStudent: function(){
     return authGet(API.getStudent);
   },
-  createClass : function ({  className, school} : any){
+  createClass : function ({ className, school } : any){
     return authPost(API.createClass, { className, school });
   }
 };
---
+++ src/api/request.ts
@@ -0,0 +1,28 @@
+import { authHeader , header } from './header';
+
+function get(url: string) {
+  return fetch(url, { method: 'GET', headers: header() })
+    .then(response => response.json());
+}
+
+function authGet(url: string) {
+  return fetch(url, { method: 'GET', headers: authHeader() })
+    .then(response => response.json());
+}
+
+function post(url: string, body: object) {
+    return fetch(url, { method: 'POST', headers: header(), body: JSON.stringify(body) })
+      .then(response => response.json());
+}
+
+function authPost(url : string, body : any) {
+  return fetch(url, { method: 'POST', headers: authHeader(), body: JSON.stringify(body) })
+  .then(response => response.json());
+}
+
+function authPut(url : string, body : any) {
+  return fetch(url, { method: 'PUT', headers: authHeader(), body: JSON.stringify(body) })
+  .then(response => response.json());
+}
+
+export { get, authGet, post, authPost, authPut };
