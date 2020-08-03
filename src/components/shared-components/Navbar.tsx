649b4ca32165411bd60eca0ab1dcb655b5b588fa
Ashish Dev <ashishdevofficial@outlook.com>
2020-08-03 13:05:54 -0500

    Changed icons of Navbar

--- src/components/shared-components/Navbar.tsx
+++ src/components/shared-components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
-import {Hints} from '../index';
+import IconGear from '../../assets/icons/icon-gear.svg';
+import Home from '../../assets/icons/icon-home.svg';
+import Game from '../../assets/icons/icon-game.svg';
+import BackPack from '../../assets/icons/icon-backpack.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
+import { Hints } from '../index';
 const Nav = styled.div`
   top: 0em;
   height: 100vh;
@@ -12,15 +21,20 @@ const Nav = styled.div`
   background: dodgerblue;
   z-index: 10;
   @media (max-width: 879px) {
-     height: 50px;
-     width: 100vw;
-     top: calc(100% - 50px);
-     grid-template-columns: 1fr 1fr 1fr 1fr;
-     grid-template-rows: 1fr;
+    height: 50px;
+    width: 100vw;
+    top: calc(100% - 50px);
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
   }
-  `;
+`;
 
-  //REMOVED DISPLAY
+const Icon = styled.img`
+ width: 2.25vw;
+ src: ${props => props.src}
+`;
+
+//REMOVED DISPLAY
 const FloatBtn = styled.button`
   position: fixed;
   bottom: 1em;
@@ -28,26 +42,53 @@ const FloatBtn = styled.button`
   border: 0;
   border-radius: 50%;
   @media (max-width: 879px) {
-      display: none;
-    }
-  `;
+    display: none;
+  }
+`;
 
-  const FloatBtn2 = styled(FloatBtn)`
-    left: 95%;
-  `;
 
-const active = {textShadow: "2px 2px 4px #000000" , color: "white"};
-const Navbar = ({showNav, hide}: {showNav: any, hide: any}):JSX.Element=> (
+// const FloatBtn2 = styled(FloatBtn)`
+//   left: 95%;
+// `;
+
+const active = { textShadow: '2px 2px 4px #000000', color: 'white' };
+const Navbar = ({
+  showNav,
+  hide,
+}: {
+  showNav: any;
+  hide: any;
+}): JSX.Element => (
   <div>
-    <Nav className={showNav? "show": "hide"}>
-      <NavLink activeStyle={active}   className="nav-item txt-md" to="/setting"><i className="fas fa-cog"></i></NavLink>
-      <NavLink activeStyle={active} className="nav-item txt-md" to="/home"><i className="fas fa-home"></i></NavLink>
-      <NavLink activeStyle={active}  className="nav-item txt-md" to="/simulation"><i className="fas fa-gamepad"></i></NavLink>
-      <NavLink activeStyle={active}  className="nav-item txt-md" to="/classes"><i className="fas fa-school"></i></NavLink>
+    <Nav className={showNav ? 'show' : 'hide'}>
+      <NavLink activeStyle={active} className="nav-item txt-md" to="/setting">
+        <Icon src={IconGear} alt="" />
+      </NavLink>
+      <NavLink activeStyle={active} className="nav-item txt-md" to="/home">
+        <Icon src={Home} alt="" />
+      </NavLink>
+      <NavLink
+        activeStyle={active}
+        className="nav-item txt-md"
+        to="/simulation"
+      >
+        <Icon src={Game} alt="" />
+      </NavLink>
+      <NavLink activeStyle={active} className="nav-item txt-md" to="/classes">
+        <Icon src={BackPack} alt="" />
+      </NavLink>
     </Nav>
-      <FloatBtn onClick={hide}><i className={ showNav? "fa fa-angle-left txt-md" : "fa fa-angle-right txt-md"}></i></FloatBtn>
-      <Hints msg={["Welcome to FinApp!","Take the Simulation when you are ready"]}/>
+    <FloatBtn onClick={hide}>
+      <i
+        className={
+          showNav ? 'fa fa-angle-left txt-md' : 'fa fa-angle-right txt-md'
+        }
+      ></i>
+    </FloatBtn>
+    <Hints
+      msg={['Welcome to FinApp!', 'Take the Simulation when you are ready']}
+    />
   </div>
-  );
+);
 
 export default Navbar;
