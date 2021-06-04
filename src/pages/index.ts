1d5639e5b8235b4152d32baa84992cff6839ae4b
Ashish Dev <ashishdevofficial@outlook.com>
2021-06-03 22:52:46 -0500

    lazy load

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,9 @@
-import Startpage from './Startpage';
-import Setting from './Setting';
-import Simulation from './Simulation';
-import Signup from './Signup';
-import AdminPanel from './AdminPanel';
+import React from 'react';
+
+const Startpage = React.lazy(() => import('./Startpage'));
+const Setting = React.lazy(() => import('./Setting'));
+const Simulation = React.lazy(() => import('./Simulation'));
+const Signup = React.lazy(() => import('./Signup'));
+const AdminPanel = React.lazy(() => import('./AdminPanel'));
 
 export { Startpage, Setting, Simulation, Signup, AdminPanel };
