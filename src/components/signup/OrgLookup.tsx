0041dc7d5e08cb37309c0cdb83960cbee3f081f9
Ashish Dev <ashishdevofficial@outlook.com>
2021-09-16 10:07:43 -0500

    organization confirmation

--- src/components/signup/OrgLookup.tsx
+++ src/components/signup/OrgLookup.tsx
@@ -10,7 +10,92 @@ import api from 'api';
 type props = {
   signUpUser: () => void;
   setSelectedOrganization: any;
-  selectedOrganization: String;
+  selectedOrganization: string;
+};
+
+const OrgConfirmation = ({
+  signUpUser,
+  setAskConfirmation,
+  selectedOrganization,
+}: {
+  signUpUser: any;
+  setAskConfirmation: any;
+  selectedOrganization: string;
+}): JSX.Element => {
+  const [checked, setChecked] = useState([false, false]);
+  const [organization, setOrganization] = useState(null);
+
+  useEffect(() => {
+    api
+      .getOrganizationDetails(selectedOrganization)
+      .then((res) => setOrganization(res))
+      .catch((err) => alert(err.message));
+  }, []);
+
+  if (!organization) return null;
+  return (
+    <div className="auth-form">
+      <div
+        className="desc-title blue-text-dark"
+        style={{ marginBottom: '0.25em' }}
+      >
+        Please confirm your organization
+      </div>
+
+      <div>
+        Address:{' '}
+        {`${organization.address}, ${organization.city}, ${organization.state}, ${organization.zip}`}
+      </div>
+      <div>Name: {organization.name}</div>
+
+      <div
+        style={{ display: 'flex', marginBottom: '0.25em', marginTop: '0.35em' }}
+      >
+        <input
+          type="checkbox"
+          id="check1"
+          name="check1"
+          checked={checked[0]}
+          style={{ alignSelf: 'center', marginRight: '0.5em' }}
+          onChange={() => setChecked([!checked[0], checked[1]])}
+        />
+        <label htmlFor="check1">Yes, This is my organization.</label>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <input
+          type="checkbox"
+          id="check2"
+          name="check2"
+          checked={checked[1]}
+          style={{ alignSelf: 'center', marginRight: '0.5em' }}
+          onChange={() => setChecked([checked[0], !checked[1]])}
+        />
+        <label htmlFor="check2">
+          I understand that I won't be able to change my organization
+          afterwards.
+        </label>
+      </div>
+      <br />
+
+      <div style={{ display: 'flex' }}>
+        <button
+          className="blue-button"
+          onClick={() => setAskConfirmation(false)}
+        >
+          Back
+        </button>
+
+        <div style={{ flexGrow: 1 }}></div>
+        <button
+          className="yellow-button"
+          disabled={checked[0] && checked[1] ? false : true}
+          onClick={() => signUpUser()}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 };
 
 /**
@@ -33,6 +118,7 @@ const OrgLookup = ({
    * set to "false" when an organization is selected
    */
   const [disableSubmit, setdisableSubmit] = useState<boolean>(true);
+  const [askConfirmation, setAskConfirmation] = useState<boolean>(false);
 
   const orgInputRef = useRef<HTMLInputElement>();
   useEffect(() => {
@@ -70,46 +156,57 @@ const OrgLookup = ({
     });
   };
 
-  return (
-    <>
-      <form className="auth-form ">
-        <div className="desc-title blue-text-dark">
-          Choose your school/organization
-        </div>
+  if (askConfirmation)
+    return (
+      <OrgConfirmation
+        signUpUser={signUpUser}
+        setAskConfirmation={setAskConfirmation}
+        selectedOrganization={selectedOrganization}
+      />
+    );
+  else
+    return (
+      <>
+        <form className="auth-form ">
+          <div className="desc-title blue-text-dark">
+            Choose your school/organization
+          </div>
 
-        <input
-          ref={orgInputRef}
-          list="organizations"
-          onInput={(e) => {
-            let val = (e.target as any).value;
-            if (val != '') {
-              setdisableSubmit(false); // Make the submit button active
+          <input
+            ref={orgInputRef}
+            list="organizations"
+            value={selectedOrganization}
+            onInput={(e) => {
+              let val = (e.target as any).value;
+
+              setdisableSubmit(val != '' ? false : true); // Make the submit button active
+
               setSelectedOrganization(val); // Update the selected organization
-            } else {
-              setdisableSubmit(true);
-            }
-          }}
-        />
-        <datalist id="organizations">{getOptions()}</datalist>
+            }}
+          />
+          <datalist id="organizations">{getOptions()}</datalist>
 
-        <button
-          className="yellow-button"
-          disabled={disableSubmit}
-          onClick={(e) => {
-            e.preventDefault();
-            if (organizations.includes(selectedOrganization)) signUpUser();
-            // API call
-            else
-              alert(
-                'Invalid Organization name! \nPlease select an organization from the dropdown list.'
-              );
-          }}
-        >
-          Submit
-        </button>
-      </form>
-    </>
-  );
+          <button
+            className="yellow-button"
+            disabled={disableSubmit}
+            onClick={(e) => {
+              e.preventDefault();
+              if (organizations.includes(selectedOrganization))
+                setAskConfirmation(true);
+              // API call
+              else {
+                alert(
+                  'Invalid Organization name! \nPlease select an organization from the dropdown list.'
+                );
+                setSelectedOrganization('');
+              }
+            }}
+          >
+            Next
+          </button>
+        </form>
+      </>
+    );
 };
 
 export default OrgLookup;
--- src/components/signup/SignupForm.tsx
+++ src/components/signup/SignupForm.tsx
@@ -27,7 +27,7 @@ const SignupForm = ({ SignupAPI }: { SignupAPI: (values: any) => void }) => {
   });
 
   const [orgListState, setOrgListState] = useState(false); // Show <OrgLookup />?
-  const [organization, setOrganization] = useState<String>(''); // User's organization
+  const [organization, setOrganization] = useState<string>(''); // User's organization
 
   const emailRef: React.RefObject<HTMLInputElement> = React.createRef();
   const cPasswordRef: React.RefObject<HTMLInputElement> = React.createRef();
